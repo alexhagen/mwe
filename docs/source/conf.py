90be8a498c311121de7caa284a9866b0b280d35c
Alex Hagen <ahagen@purdue.edu>
2017-05-17 19:38:56 -0400

    got it working with the new sphinx-themes install

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,10 @@ import os
 import shlex
 import sphinx_bootstrap_theme
 
+def setup(app):
+    app.add_stylesheet('bootstrap.css')
+    app.add_stylesheet('extra.css')
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
